859ea00a9dd001b559ae9d4c4de3b10e13d3a65d
Ashish <ashish@a.patel7734@gmail.com>
2015-04-29 15:06:07 -0700

    add mapview + display current location

--- On the Map/MapViewController.swift
+++ On the Map/MapViewController.swift
@@ -7,13 +7,18 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
+    
+    
+    @IBOutlet weak var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        mapView.showsUserLocation = true
+
     }
 
     override func didReceiveMemoryWarning() {
